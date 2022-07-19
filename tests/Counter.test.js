1351d548654e90f27b22aa7349a291c831028b75
Marcin Kołodziej <marcin1.kolodziej@gmail.com>
2022-07-19 10:46:34 +0200

    All counter methods and logic tested

--- tests/Counter.test.js
+++ tests/Counter.test.js
@@ -1,9 +1,17 @@
 import Counter from '../src/Counter'
 import DecreasingCounter from '../src/DecreasingCounter'
 
+const originalLogFn = console.log
+
+beforeAll(() => console.log = jest.fn())
+afterAll(() => console.log = originalLogFn)
+
+beforeEach(() => console.log.mockReset())
+
 describe('Counter', () => {
 
-    describe('buisness logic - increasing, decreasing', () => {
+
+    describe('class public fields', () => {
 
         it('Counter should be an ES6 class instance', () => {
 
@@ -39,7 +47,86 @@ describe('Counter', () => {
 
         })
 
+    })
 
+    describe('buisness logic - increasing, decreasing', () => {
+
+        it('should start from 0', () => {
+
+            const counter1 = new Counter('body')
+
+            expect(counter1.number).toBe(0)
+
+        })
+
+        it('should start from specified value if is passed second arg', () => {
+
+            const counter1 = new Counter('body', 123)
+
+            expect(counter1.number).toBe(123)
+
+        })
+
+        it('should have `.inc` method which increasing stored number by one', () => {
+
+            const counter1 = new Counter('body')
+
+            expect(counter1.inc).toBeDefined()
+            expect(counter1.inc).toBeInstanceOf(Function)
+            expect(() => counter1.inc()).not.toThrow()
+
+            expect(counter1.number).toBe(1)
+
+        })
+
+        it('should have `.inc` method which increasing stored number by one if start value is specified', () => {
+
+            const counter1 = new Counter('body', 123)
+
+            counter1.inc()
+
+            expect(counter1.number).toBe(124)
+
+        })
+
+        it('should have status method which returned stored value', () => {
+
+            const counter1 = new Counter('body')
+            const counter2 = new Counter('body', 123)
+
+            expect(counter1.status()).toBe(counter1.number)
+            expect(counter2.status()).toBe(counter2.number)
+
+        })
+
+        it('should have status method that console.log stored value', () => {
+
+            const counter1 = new Counter('body')
+
+            counter1.status()
+
+            expect(console.log).toHaveBeenCalled()
+            expect(console.log).toHaveBeenCalledTimes(1)
+            expect(console.log.mock.calls[0]).toEqual(['Current number is: 0'])
+
+        })
+
+        it('should have `.toString` method that informs about stored value', () => {
+
+            const counter1 = new Counter('body')
+
+            expect(counter1.toString()).toBe('Current number is: 0')
+
+        })
+
+        it('should have `.valueOf` method that returns stored value', () => {
+
+            const counter1 = new Counter('body')
+
+            expect(counter1.valueOf()).toBe(0)
+            expect(counter1.valueOf()).toBe(counter1.number)
+
+        })
     })
 
 })
